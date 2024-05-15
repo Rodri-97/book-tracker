c448950aeea1b3783b0a2ba9208a53f9b90355bc
Rodrigo Coelho <rodrigo_coelho@orange.fr>
2024-05-15 21:39:41 +0200

    improve ui in book details page

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -1,7 +1,13 @@
 import { GoogleAPIBook } from "@/lib/interfaces";
 import axios from "axios";
 import DOMPurify from "isomorphic-dompurify";
-import UserButtonsContainer from "./_components/user-buttons-container";
+import RemoveBookModal from "@/components/remove-book-modal";
+import { validateRequest } from "@/lib/utils.server";
+import { getBookByIds } from "@/services/books.service";
+import { Book } from "@prisma/client";
+import UserBookRating from "@/components/user-book-rating";
+import BookStatus from "@/components/book-status";
+import AddBookButton from "./_components/add-book-button";
 
 export default async function BookDetails({
   params,
@@ -12,26 +18,52 @@ export default async function BookDetails({
     .get(`https://www.googleapis.com/books/v1/volumes/${params.id}`)
     .catch(() => null);
 
-  const book: GoogleAPIBook | null = res?.data;
+  const bookGoogleData: GoogleAPIBook | null = res?.data;
 
-  if (!book) return <div>Book not found.</div>;
+  if (!bookGoogleData) return <div>Book not found.</div>;
 
-  const { title, authors, description } = book.volumeInfo;
-  const image = book.volumeInfo.imageLinks
-    ? book.volumeInfo.imageLinks.thumbnail
+  const { title, authors, description } = bookGoogleData.volumeInfo;
+  const image = bookGoogleData.volumeInfo.imageLinks
+    ? bookGoogleData.volumeInfo.imageLinks.thumbnail
     : "/covers/unavailable-cover.jpg";
+
+  const { user } = await validateRequest();
+
+  let book: Book | null = null;
+
+  if (user) {
+    book = await getBookByIds({ googleId: bookGoogleData.id, userId: user.id });
+  }
 
   return (
     <div>
       <article className="bg-white rounded-lg p-6 flex flex-col justify-center items-center gap-12">
-        <section className="flex flex-col justify-center items-center gap-6 lg:flex-row">
-          <img src={image} className="w-60 h-72 rounded-lg" alt={title} />
-          <div className="flex flex-col text-center gap-6 lg:text-left lg:gap-2">
-            <h1 className="font-bold text-2xl">{title}</h1>
-            <h2 className="text-xl text-slate-700">{authors?.join(", ")}</h2>
+        {user && book ? (
+          <div className="ml-auto mb-auto">
+            <RemoveBookModal googleId={bookGoogleData.id} />
           </div>
+        ) : null}
+
+        <section className="flex flex-col justify-center items-center gap-6 lg:flex-row">
+          <div className="flex flex-col justify-center items-center gap-[1px]">
+            <h1 className="font-bold text-2xl">{title}</h1>
+            <h2 className="text-xl text-slate-700 mb-4">
+              {authors?.join(", ")}
+            </h2>
+            <img src={image} className="w-60 h-72 rounded-lg" alt={title} />
+          </div>
+          {user ? (
+            book ? (
+              <div className="flex flex-col gap-4">
+                {book.status === "READ" ? <UserBookRating book={book} /> : null}
+                <BookStatus book={book} />
+              </div>
+            ) : (
+              <AddBookButton googleId={bookGoogleData.id} />
+            )
+          ) : null}
         </section>
-        <UserButtonsContainer googleId={book.id} />
+
         <hr className="h-[2px] w-full bg-slate-300" />
         <section
           dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(description) }}
